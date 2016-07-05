62e00a365c75e40000a28c73eeb68ff1dd14aaaf
Impuls3iv <sammy.unterman@gmail.com>
2016-07-05 15:26:07 -0400

    completed

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
+$(document).on('ready', function(){
+  $('.on').on('click', whatHappensWhenOn)
+});
 
+function whatHappensWhenOn(){
+  $('.on').removeClass('on').addClass('off')
+  $('body').removeClass('light').addClass('dark')
+  $('h1').html('Its so dark in here')
+    $('.off').on('click', whatHappensWhenOff)
+      $('.on').off('click')
+};
 
+function whatHappensWhenOff(){
+  $('.off').removeClass('off').addClass('on')
+  $('body').removeClass('dark').addClass('light')
+  $('h1').html('Its so bright in here')
+    $('.on').on('click', whatHappensWhenOn)
+      $('.off').off('click')
+};
